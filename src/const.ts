8d1e360cf489790267424bba0193310a32eea959
Noisekit <nikita@cc.snxdao.io>
2023-05-23 14:27:47 +0800

    Update cluster config IPNS

--- src/const.ts
+++ src/const.ts
@@ -1,5 +1,5 @@
 export const SYNTHETIX_IPNS =
-  'k51qzi5uqu5dmdzyb1begj16z2v5btbyzo1lnkdph0kn84o9gmc2uokpi4w54c';
+  'k51qzi5uqu5dj0vqsuc4wyyj93tpaurdfjtulpx0w45q8eqd7uay49zodimyh7';
 
 export const SYNTHETIX_NODE_APP_CONFIG =
   'k2k4r8jhn5r54sf708bnlxun971q0pteab82iw98544sj1e0hi3hp9q6';
