7fdbda4e6bee9bb08fe0aad30d5588c11499cfd6
Noisekit <nikita@cc.snxdao.io>
2024-03-11 18:05:24 +0800

    Lint harder

--- src/main/util.js
+++ src/main/util.js
@@ -1,31 +1,36 @@
 export function getPlatformDetails() {
   const targetArch = (() => {
     switch (process.arch) {
-      case "x64":
-        return "amd64";
-      case "arm64":
+      case 'x64':
+        return 'amd64';
+      case 'arm64':
+        return 'arm64';
       default:
         return process.arch;
     }
   })();
   const osPlatform = (() => {
     switch (process.platform) {
-      case "win32":
-        return "windows";
-      case "darwin":
-      case "linux":
+      case 'win32':
+        return 'windows';
+      case 'darwin':
+        return 'darwin';
+      case 'linux':
+        return 'linux';
       default:
         return process.platform;
     }
   })();
   const fileExt = (() => {
     switch (process.platform) {
-      case "win32":
-        return "zip";
-      case "darwin":
-      case "linux":
+      case 'win32':
+        return 'zip';
+      case 'darwin':
+        return 'tar.gz';
+      case 'linux':
+        return 'tar.gz';
       default:
-        return "tar.gz";
+        return 'tar.gz';
     }
   })();
   return { osPlatform, fileExt, targetArch };
