63bd07268b33bf3449ab1f292d0dd0ed04c39199
dbeal <git@dbeal.dev>
2024-03-11 14:02:14 +0900

    support for linux, support for building all on linux (#35)
    * support for linux, support for building all on linux
    
    changes summary:
    * better platform recognition for all 3 platforms
    * add sample circleci which builds and pushes artifacts for all 3 platforms
    * build command tries to build for all 3 platforms (if a platform is unsupported, it just doesn't build it)
    
    * syntax error
    
    * ci fix
    
    * ci tune
    
    * undo circle ci
    
    * Update package.json
    
    * Simplify getPlatformDetails
    
    ---------
    
    Co-authored-by: Noisekit <nikita@cc.snxdao.io>

--- src/main/util.ts
+++ src/main/util.ts
@@ -1,6 +1,5 @@
 import { URL } from 'url';
 import path from 'path';
-import os from 'os';
 
 export function resolveHtmlPath(htmlFileName: string) {
   if (process.env.NODE_ENV === 'development') {
@@ -13,9 +12,34 @@ export function resolveHtmlPath(htmlFileName: string) {
 }
 
 export function getPlatformDetails() {
-  const arch = os.arch();
-  const targetArch = arch === 'x64' ? 'amd64' : 'arm64';
-  const osPlatform = process.platform === 'darwin' ? 'darwin' : 'windows';
-  const fileExt = osPlatform === 'darwin' ? 'tar.gz' : 'zip';
+  const targetArch = (() => {
+    switch (process.arch) {
+      case 'x64':
+        return 'amd64';
+      case 'arm64':
+      default:
+        return process.arch;
+    }
+  })();
+  const osPlatform = (() => {
+    switch (process.platform) {
+      case 'win32':
+        return 'windows';
+      case 'darwin':
+      case 'linux':
+      default:
+        return process.platform;
+    }
+  })();
+  const fileExt = (() => {
+    switch (process.platform) {
+      case 'win32':
+        return 'zip';
+      case 'darwin':
+      case 'linux':
+      default:
+        return 'tar.gz';
+    }
+  })();
   return { osPlatform, fileExt, targetArch };
 }
