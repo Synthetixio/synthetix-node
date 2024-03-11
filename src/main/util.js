49acca8b246c4ed096f284cd4492f83aac11a197
Noisekit <nikita@cc.snxdao.io>
2024-03-11 13:06:20 +0800

    Linux support

--- src/main/util.js
+++ src/main/util.js
@@ -1,9 +1,32 @@
-import os from 'os';
-
 export function getPlatformDetails() {
-  const arch = os.arch();
-  const targetArch = arch === 'x64' ? 'amd64' : 'arm64';
-  const osPlatform = process.platform === 'darwin' ? 'darwin' : 'windows';
-  const fileExt = osPlatform === 'darwin' ? 'tar.gz' : 'zip';
+  const targetArch = (() => {
+    switch (process.arch) {
+      case "x64":
+        return "amd64";
+      case "arm64":
+      default:
+        return process.arch;
+    }
+  })();
+  const osPlatform = (() => {
+    switch (process.platform) {
+      case "win32":
+        return "windows";
+      case "darwin":
+      case "linux":
+      default:
+        return process.platform;
+    }
+  })();
+  const fileExt = (() => {
+    switch (process.platform) {
+      case "win32":
+        return "zip";
+      case "darwin":
+      case "linux":
+      default:
+        return "tar.gz";
+    }
+  })();
   return { osPlatform, fileExt, targetArch };
 }
