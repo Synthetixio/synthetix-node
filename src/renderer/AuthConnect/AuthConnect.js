882c18e06e6b4bbc5cd327c3e0dd2e75fd1004a4
Volodymyr Derunov <vderunov@gmail.com>
2024-11-08 04:16:33 +0200

    Add if condition for signedMessage

--- src/renderer/AuthConnect/AuthConnect.js
+++ src/renderer/AuthConnect/AuthConnect.js
@@ -1,15 +1,15 @@
 const React = require('react');
-const {useMutation} = require('@tanstack/react-query');
+const { useMutation } = require('@tanstack/react-query');
 const {
   useAppKitAccount,
   useDisconnect,
   useAppKit,
   useAppKitProvider,
 } = require('@reown/appkit/react');
-const {Spinner, Text, Box, Flex, Button, Stack, useColorModeValue} = require('@chakra-ui/react');
-const {usePermissions} = require('./usePermissions');
-const {getApiUrl, saveToken} = require('./utils');
-const {BrowserProvider} = require('ethers');
+const { Spinner, Text, Box, Flex, Button, Stack, useColorModeValue } = require('@chakra-ui/react');
+const { usePermissions } = require('./usePermissions');
+const { getApiUrl, saveToken } = require('./utils');
+const { BrowserProvider } = require('ethers');
 
 // TODO make as env
 const API_URL = 'http://45.146.7.38:3005/';
@@ -36,27 +36,28 @@ const makeUnauthenticatedRequest = async (endpoint, data) => {
 
 function AuthConnect() {
   const [token, setToken] = React.useState();
-  const {address: walletAddress, isConnected} = useAppKitAccount();
-  const {disconnect} = useDisconnect();
-  const {open} = useAppKit();
-  const {walletProvider} = useAppKitProvider('eip155');
+  const { address: walletAddress, isConnected } = useAppKitAccount();
+  const { disconnect } = useDisconnect();
+  const { open } = useAppKit();
+  const { walletProvider } = useAppKitProvider('eip155');
   const permissions = usePermissions();
 
   const signupMutation = useMutation({
     mutationFn: (data) => makeUnauthenticatedRequest('signup', data),
-    onSuccess: async ({nonce}) => {
+    onSuccess: async ({ nonce }) => {
       const provider = new BrowserProvider(walletProvider);
       const signer = await provider.getSigner();
-      await signer?.signMessage(nonce).then((signedMessage) => {
-        verificationMutation.mutate({nonce, signedMessage});
-      });
+      const signedMessage = await signer?.signMessage(nonce);
+      if (signedMessage) {
+        verificationMutation.mutate({ nonce, signedMessage });
+      }
     },
   });
 
   const verificationMutation = useMutation({
     mutationFn: (data) => makeUnauthenticatedRequest('verify', data),
-    onSuccess: ({token}) => {
-      saveToken({walletAddress, token});
+    onSuccess: ({ token }) => {
+      saveToken({ walletAddress, token });
       setToken(token);
     },
   });
@@ -66,22 +67,22 @@ function AuthConnect() {
       <Box>
         <Flex
           minH={'60px'}
-          py={{base: 2}}
-          px={{base: 4}}
+          py={{ base: 2 }}
+          px={{ base: 4 }}
           borderBottom={1}
           borderStyle={'solid'}
           borderColor={useColorModeValue('gray.200', 'gray.900')}
           align={'center'}
           justify={'flex-end'}
         >
-          <Stack flex={{base: 1, md: 0}} direction={'row'} spacing={6}>
-            <Button onClick={() => open({view: 'Networks'})}>Open Network Modal</Button>
+          <Stack flex={{ base: 1, md: 0 }} direction={'row'} spacing={6}>
+            <Button onClick={() => open({ view: 'Networks' })}>Open Network Modal</Button>
             {isConnected && token ? (
               <Button onClick={() => console.log('Log Out')}>Log Out</Button>
             ) : null}
             {isConnected && !token ? (
               <>
-                <Button onClick={() => signupMutation.mutate({walletAddress})}> Log In</Button>
+                <Button onClick={() => signupMutation.mutate({ walletAddress })}> Log In</Button>
                 <Button onClick={disconnect}>Disconnect</Button>
               </>
             ) : null}
@@ -93,9 +94,9 @@ function AuthConnect() {
       </Box>
 
       {permissions.isFetching ? (
-        <Spinner/>
+        <Spinner />
       ) : permissions.data?.isGranted && token ? (
-        <input/>
+        <input />
       ) : (
         <Text>Please login and request access permissions to use</Text>
       )}
@@ -103,4 +104,4 @@ function AuthConnect() {
   );
 }
 
-module.exports = {AuthConnect};
+module.exports = { AuthConnect };
